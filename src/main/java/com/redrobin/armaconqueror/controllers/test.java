b470d06897560daa0fc5ca82b523317ac5116ce4
RedRobin84 <steel_martin@seznam.cz>
2020-06-07 12:39:10 +0200

    - initial setup data

---
+++ src/main/java/com/redrobin/armaconqueror/controllers/test.java
@@ -0,0 +1,31 @@
+package com.redrobin.armaconqueror.controllers;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+public class test {
+
+    private String firstName = "John";
+    private String lastName = "Doe";
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String showGreeting() {
+        return "Hello " + firstName + " " + lastName + "!";
+    }
+}
---
+++ src/test/java/com/redrobin/armaconqueror/ArmaConquerorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redrobin.armaconqueror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmaConquerorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
